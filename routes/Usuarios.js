2bbf7d7af4785929bff586a97cb9638adefe51fd
crislozanor05 <cristina.lozanor05@alumnostrazos.net>
2026-07-03 17:54:55 +0200

    ultimos cambios

--- routes/Usuarios.js
+++ routes/Usuarios.js
@@ -9,17 +9,14 @@ const router = express.Router();
 
 // POST /usuarios/registro
 router.post("/registro", async function (req, res) {
-  const user=req.body 
-  console.log(user)
-// const db = obtenerDB()
-req.app.locals.db.collection("usuarios").insertOne(user)
-res.send(user)
-})
+  const user = req.body;
+  console.log(user);
+  // const db = obtenerDB()
+  req.app.locals.db.collection("usuarios").insertOne(user);
+  res.send(user);
+});
 
 router.get("/usuarios", async function (req, res) {
-res.send("hola desde usuarios")
-})
-module.exports =  router ;
-
-
-
+  res.send("hola desde usuarios");
+});
+module.exports = router;
